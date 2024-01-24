551630af4a157aa0351b272993dec25fe4b93fc6
koruzzo <raphael.tr.jarreau@gmail.com>
2024-01-24 15:04:19 +0100

    Add some modifications and fix the ',' bug

--- calculatrice.py
+++ calculatrice.py
@@ -1,7 +1,7 @@
 """
     tkinter est utilisé pour la construction et l'affichage de l'application
     pandas est utilisé pour le travail sur csv et le calcul
-    messagebox de tkinter est utilisé pour générer des boite de dialogue
+    messagebox de tkinter est utilisé pour générer des boites de dialogue
 """
 import tkinter as tk
 from tkinter import messagebox
@@ -16,8 +16,8 @@ def calc_and_err():
     """
     try:
         expression = entry.get()
+        expression = replace_commas(expression)
         result = pd.eval(expression)
-        result = round(result, 4)  # On limite le résultat à 4 chiffres après la virgule
         result_label.config(text = "Résultat : " + str(result))
         save_operation(expression, result)  # On sauvegarde l'opération
     except ZeroDivisionError:
@@ -35,6 +35,23 @@ def calc_and_err():
     except ImportError as e:
         messagebox.showerror("Erreur", f"Erreur de calcul : {str(e)}")
         result_label.config(text = "Erreur")
+
+def replace_commas(expression):
+    """
+    _summary_
+        Cette fonction remplace les ',' entrées, par des points
+    Args:
+        expression (_str_): _L'expression mathématique entrée par l'utilisateur_
+
+    Returns:
+        parts (_str_): _L'expression mathématique entrée par l'utilisateur, modifiée_
+    """
+    parts = expression.split()
+    for i, part in enumerate(parts):
+        if '.' not in part and ',' in part:
+            parts[i] = part.replace(',', '.')
+    return ' '.join(parts)
+
 def save_operation(expression, result):
     """
     _summary_
@@ -44,7 +61,7 @@ def save_operation(expression, result):
 
     Args:
         expression (_str_): _L'expression mathématique entrée par l'utilisateur_
-        result (_float_): _Le résultat de l'évaluation de l'expression_
+        result (_float_): _Le résultat de l'expression_
     """
     try:
         data = pd.read_csv('operations.csv')
